ece36a385ca4384f8855f2c64ba119ca2d522791
Andy <159312225+Mw4mba@users.noreply.github.com>
2024-08-04 22:39:22 +0200

    Update script.js
    Signed-off-by: Andy <159312225+Mw4mba@users.noreply.github.com>

--- python/script.js
+++ python/script.js
@@ -21,3 +21,22 @@ document.getElementById('next-button').addEventListener('click',
 window.onload = () => {
     document.getElementById('next-button').click(); //Automatic
 };
+
+
+document.getElementById('previous-button').addEventListener('click',
+    function () {
+        if (currentExercise>0 && currentExercise<=exercises.length) {
+            const exercise = exercises[currentExercise];
+            document.getElementById('exercise-content').innerHTML =
+                    `<h2>${exercise.title}</h2><p>${exercise.content}</p>`;
+            if (exercise.questions) {
+                exercise.questions.forEach(question => {
+                    document.getElementById('exercise-content').innerHTML += `<div><b>Question:</b> ${question}</div>`;
+                });
+            }
+            currentExercise--;
+        } 
+    });
+window.onload = () => {
+document.getElementById('next-button').click(); //Automatic
+};
